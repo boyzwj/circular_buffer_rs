1993b9507a8e0b580b8ebf1f760ecadd04b8c360
tom <tom@illusioncolors.com>
2024-06-10 04:58:11 +0800

    add benchmark

--- bench/add.exs
+++ bench/add.exs
@@ -5,4 +5,8 @@ CircularBufferRs.new(1, 32)
 Benchee.run(%{
   "circular_buffer_add" => fn ->CircularBuffer.insert(a,"gdfgdgdfgdfgdfgdfgdgdgd") end,
   "circular_buffer_rs_add" => fn -> CircularBufferRs.push(1, "gdfgdgdfgdfgdfgdfgdgdgd") end
-})
+},
+  warmup: 1,
+  time: 5,
+  memory_time: 2,
+  reduction_time: 2)
