9f6e76b8fbe5737a1bd0d3bb016ff1e5683d5fd8
tom <tom@illusioncolors.com>
2024-06-30 02:16:29 +0800

    remove windows support

--- lib/native/native.ex
+++ lib/native/native.ex
@@ -8,8 +8,16 @@ defmodule CircularBufferRs.Native do
     crate: "circular_buffer_rs",
     base_url: "#{github_url}/releases/download/v#{version}",
     force_build: System.get_env("FORCE_CIRCULAR_BUFFER_RS_BUILD") in ["1", "true"],
-    targets:
-      Enum.uniq(["aarch64-unknown-linux-musl" | RustlerPrecompiled.Config.default_targets()]),
+    targets: [
+      "aarch64-apple-darwin",
+      "aarch64-unknown-linux-gnu",
+      "aarch64-unknown-linux-musl",
+      "arm-unknown-linux-gnueabihf",
+      "riscv64gc-unknown-linux-gnu",
+      "x86_64-apple-darwin",
+      "x86_64-unknown-linux-gnu",
+      "x86_64-unknown-linux-musl"
+    ],
     # nif_versions: ["2.16"],
     version: version
 
