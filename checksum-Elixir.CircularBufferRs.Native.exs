657ebe7e297914c2264fa416b18ddfe8f404ad38
tom <tom@illusioncolors.com>
2024-07-01 01:14:41 +0800

    upgrade checksum

--- checksum-Elixir.CircularBufferRs.Native.exs
+++ checksum-Elixir.CircularBufferRs.Native.exs
@@ -1,12 +1,8 @@
 %{
-  "circular_buffer_rs-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:f0df63d5e5741fb66d7a68b44002b5421f3d6a346cb6c9124fcb3951dddf5b10",
-  "circular_buffer_rs-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:bb1846617ab66d2db6612f615bd7fc91d261cbbd7c1fc77d503f404539deafe0",
-  "libcircular_buffer_rs-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:16d460af5b19c410b8c6f6eb1e948ddfe010ac469bc565b21747c9267a5b496f",
-  "libcircular_buffer_rs-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:09ba7eaaea41afe2cf323b689bd66bf0b09b9c930768d75afe7208b13a7ccf8a",
-  "libcircular_buffer_rs-v0.1.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:55ca0e5cdc6cb0008507dc83bdbf665f68002cccf62a36ff97b5d575e78ec581",
-  "libcircular_buffer_rs-v0.1.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:98d96ad41e6f65ae058fa0a3a34c24fa91f652cee980136d566ddc1abfea505e",
-  "libcircular_buffer_rs-v0.1.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:c4eab5f14a36b04df9136fb6179d30f77e9ea984f8e91306aca20dcc0f404047",
-  "libcircular_buffer_rs-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:c1b85096591adcdd0d5d0f6f9b7a5600bc973af88b102561ccc1e1577ce370a2",
-  "libcircular_buffer_rs-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:bb506950a7a11802f34dd92845ef14322546a335e5b5ec0203f7eeb146af9be3",
-  "libcircular_buffer_rs-v0.1.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:79c64436854cc6321649f665c149cc97c7aac12da807d330fa9706880418afe1",
+  "libcircular_buffer_rs-v0.2.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:f197b70b346e8cba3e51f6b388cca5c04ba4a6ccaec6c092119f374304ecd3f2",
+  "libcircular_buffer_rs-v0.2.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:66500a298ca0246fa39a58f9a204713296d774d0e7ee71b66a86dfe244a48180",
+  "libcircular_buffer_rs-v0.2.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:63c05ff018d67023ec4f2da2601dea222c8a290ddb2f6c1c00552704a07e80b7",
+  "libcircular_buffer_rs-v0.2.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:89a8c363aff174462c0b840b79aa6b574a3918076243ee817a5facfaa1916cfe",
+  "libcircular_buffer_rs-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:3b8e73e1fe8cc374b41fa7a16aa64b2d60334af5865d01251061e4f4d619b2ca",
+  "libcircular_buffer_rs-v0.2.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:58813239e83f238291b67480dda355e2c019b7112096b9e955aa8f1c20e60fa8",
 }
