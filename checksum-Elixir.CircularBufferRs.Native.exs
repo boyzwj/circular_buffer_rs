230ddf519107866cf937def3be10a0073218351a
tom <boyzwj@qq.com>
2024-07-08 01:36:15 +0800

    add checksum

---
+++ checksum-Elixir.CircularBufferRs.Native.exs
@@ -0,0 +1,10 @@
+%{
+  "circular_buffer_rs-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:c8df1ef713d773ee1c842b3abfb499eadf324e51e8e452d686bb60f3332b7d42",
+  "circular_buffer_rs-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:2a7865c578cd54a0372dde08d0c22d5944c79e396043b5b1ba05cc53a596f46c",
+  "libcircular_buffer_rs-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:497e836a8d377bb1c0c8324e68a3662b1b62d89227169a1e8e1684215aee069e",
+  "libcircular_buffer_rs-v0.1.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:d982ff2885565d689533864a5110cd56bf367a09033ccaa7e5baffb500dcbe53",
+  "libcircular_buffer_rs-v0.1.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:d39f0c6b0a45efa0ceafcfcb2228341a38c02d15091ab84873dbde3a112e101a",
+  "libcircular_buffer_rs-v0.1.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:63e6a51241d504019c65826a9c172876ba94a883bd13ae833f6af0f1d4d2c606",
+  "libcircular_buffer_rs-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:1bce7ed92afdd4fc82596b3d81d2ab66adf87158f979b3a7f04a8309220dc575",
+  "libcircular_buffer_rs-v0.1.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:019c2fb4f13644bc1608fcf4ad95de2526b77f831220a8ad39dd2fba43ec7d4e",
+}
